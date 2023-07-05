55be7027884c13f7cb1ae390e42278294d9823f7
s0ulb0lt <pranay.db21@gmail.com>
2023-07-04 21:17:52 -0700

    Initial commit

---
+++ src/main/java/net/soulbolt/oresnmore/OresNMoreClient.java
@@ -0,0 +1,10 @@
+package net.soulbolt.oresnmore;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OresNMoreClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
